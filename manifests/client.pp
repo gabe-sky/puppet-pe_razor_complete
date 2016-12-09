f4f1485a678836cb8d3eb8a4f35d583dc3c3040a
Kris Reese <kris.reese@wwt.com>
2016-12-09 10:24:07 -0600

    Allows better control over pe-razor-client install

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,17 +1,21 @@
 # This simple class just makes sure the pe-razor-client gem is installed.  This
 # provides the `razor` command line interface.
 
-class pe_razor_complete::client {
+class pe_razor_complete::client (
+  $provider    = 'gem',
+  $system_ruby = true,
+) {
 
-  # In case someone else is managing ruby and gems on this system, we'll use
-  # ensure_packages from the stdlib to be safe about trying to manage them.
-  # But we do need them to be installed.
-  ensure_packages( ['ruby','rubygems'], {'ensure' => 'installed' } )
+  if $system_ruby {
+    # In case someone else is managing ruby and gems on this system, we'll use
+    # ensure_packages from the stdlib to be safe about trying to manage them.
+    ensure_packages( ['ruby','rubygems'], {'ensure' => 'installed' } )
+  }
 
   # The razor command line tool is just a ruby gem.
   package { 'pe-razor-client':
     ensure   => installed,
-    provider => gem,
+    provider => $provider,
   }
 
 }
